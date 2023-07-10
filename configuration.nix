cd2e8ff312aa572d9b557187f972b203946712fa
Archie <51773524+archieplaylist@users.noreply.github.com>
2023-07-10 13:41:17 +0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,20 +1,26 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
-{
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+let
+  # Import unstable channel.
+  # sudo nix-channel --add http://nixos.org/channels/nixpkgs-unstable nixpkgs-unstable
+  # sudo nix-channel --update nixpkgs-unstable
+  unstable = import <nixos-unstable> { config.allowUnfree = true; };
+
+in {
+  imports = [ ./hardware-configuration.nix ];
+
+  # sudo dd if=/dev/zero of=/var/lib/swapfile bs=1M count=4k status=progress
+  swapDevices = [ {
+    device = "/var/lib/swapfile";
+    size = 4*1024;
+  } ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  #boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixosvm"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -45,9 +51,28 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  # GNOME
+  #  services.xserver.displayManager.gdm.enable = true;
+  #  services.xserver.desktopManager.gnome.enable = true;
+
+  # XFCE 
+  # services.xserver = {
+  #   enable = true;
+  #   desktopManager = {
+  #     xterm.enable = false;
+  #     xfce.enable = true;
+  #   };
+  #   displayManager.defaultSession = "xfce";
+  # };
+
+  # KDE PLASMA
+  services.xserver = {
+    desktopManager = { plasma5.enable = true; };
+    displayManager = { sddm.enable = true; };
+    displayManager.defaultSession = "plasmawayland";
+  };
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [ oxygen ];
+  programs.dconf.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
@@ -55,8 +80,17 @@
     xkbVariant = "";
   };
 
+  services.xserver.videoDrivers = [ "qcl" ];
+
+  hardware.opengl.driSupport32Bit = true;
+
+  # hardware.opengl.extraPackages = [ rocm-opencl-icd ];
+
+  services.qemuGuest.enable = true;
+  services.spice-vdagentd.enable = true;
+
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  # services.printing.enable = true;
 
   # Enable sound with pipewire.
   sound.enable = true;
@@ -67,7 +101,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
@@ -82,11 +115,13 @@
   users.users.mario = {
     isNormalUser = true;
     description = "mario";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
     packages = with pkgs; [
-      firefox
-      kate
-    #  thunderbird
+      unstable.firefox
+      unstable.brave
+      unstable.vscode
+      unstable.onlyoffice-bin
+      unstable.microsoft-edge
     ];
   };
 
@@ -96,22 +131,54 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    git
+    gparted
+    htop
+    neofetch
+    nixfmt
+    vim
+    wget
+    zsh
+    # VM
+    # unstable.spice-vdagent # virt manager guest
   ];
+
+  fonts = {
+    fontDir.enable = true;
+    fonts = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+      corefonts
+    ];
+  };
+
+  # fonts.fontDir.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
+  services.flatpak.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-kde ];
+  };
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    # settings.PermitRootLogin = no;
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
