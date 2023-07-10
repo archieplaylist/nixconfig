e55631aa19705b65ff458809ce6e766296f27b73
Archie <51773524+archieplaylist@users.noreply.github.com>
2023-07-10 14:57:48 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "flake power";
+  description = "flakes power";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
@@ -41,39 +41,36 @@
             ./home/shared
 
           
-            lanzaboote.nixosModules.lanzaboote
-            ({ pkgs, lib, ... }: {
-                boot.bootspec.enable = true;
-          
-                environment.systemPackages = [
-              pkgs.sbctl
-                ];
-          
-                boot.loader.systemd-boot.enable = lib.mkForce false;
-          
-                boot.lanzaboote = {
-              enable = true;
-              pkiBundle = "/etc/secureboot";
-                };
-            })
-
-            {
-              nixpkgs.overlays = [
-                # nixpkgs-f2k.overlays.compositors
-                # nixpkgs-f2k.overlays.window-managers
-                nixpkgs-f2k.overlays.stdenvs
-
-                (final: _:
-                  let
-                    inherit (final) system;
-                  in
-                  {
-                    patched-fonts = pkgs.callPackage ./derivations/patched-fonts.nix {
-                      src = inputs.patched-fonts;
-                    };
-                  })
+          lanzaboote.nixosModules.lanzaboote
+          ({ pkgs, lib, ... }: {
+              boot.bootspec.enable = true;
+              environment.systemPackages = [
+                pkgs.sbctl
               ];
-            }
+              boot.loader.systemd-boot.enable = lib.mkForce false;
+              boot.lanzaboote = {
+                enable = true;
+                pkiBundle = "/etc/secureboot";
+              };
+          })
+
+          {
+            nixpkgs.overlays = [
+              # nixpkgs-f2k.overlays.compositors
+              # nixpkgs-f2k.overlays.window-managers
+              nixpkgs-f2k.overlays.stdenvs
+
+              (final: _:
+                let
+                  inherit (final) system;
+                in
+                {
+                  patched-fonts = pkgs.callPackage ./derivations/patched-fonts.nix {
+                    src = inputs.patched-fonts;
+                  };
+                })
+            ];
+          }
 
             home-manager.nixosModules.home-manager
             {
