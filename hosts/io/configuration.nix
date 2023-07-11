09eed92eab283beba9b310a4004c9b2fe471cf40
Archie <51773524+archieplaylist@users.noreply.github.com>
2023-07-11 14:46:00 +0700

    Update configuration.nix

--- hosts/io/configuration.nix
+++ hosts/io/configuration.nix
@@ -30,24 +30,49 @@
 #
     # DESKTOP ENVIRONTMENT
     displayManager = {
-      sddm = {
+      gdm = {
         enable = true;
       };
-      defaultSession = "plasmawayland";
+      # defaultSession = "plasmawayland";
     };
     desktopManager = { 
-      plasma5.enable = true; 
+      gnome.enable = true; 
       };
   };
 
-  nixpkgs.config.firefox.enablePlasmaBrowserIntegration = true;
+  services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+  services.gnome.games.enable = true;
 
-  environment.plasma5.excludePackages = with pkgs.libsForQt5; [ oxygen ];
+  environment.gnome.excludePackages = (with pkgs; [
+      # gnome-photos
+      gnome-tour
+  ]) ++ (with pkgs.gnome; [
+      cheese # webcam tool
+      gnome-music
+      # gnome-terminal
+      gedit # text editor
+      epiphany # web browser
+      geary # email reader
+      evince # document viewer
+      gnome-characters
+      totem # video player
+      tali # poker game
+      iagno # go game
+      hitori # sudoku game
+      atomix # puzzle game
+  ]);
+
+  environment.systemPackages = [
+    gnomeExtensions.dash-to-dock
+    # gnomeExtensions.gsconnect
+    # gnomeExtensions.mpris-indicator-button
+  ];
+
   programs.dconf.enable = true;
 
   xdg.portal = {
     enable = true;
-    extraPortals = with pkgs; [ xdg-desktop-portal-kde ];
+    extraPortals = with pkgs; [ xdg-desktop-portal-gnome ];
   };
 
   environment.variables = {
