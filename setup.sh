349cf0cfe7c73881490712466500356ee4063ff8
archieplaylist <mario.tani25@gmail.com>
2023-08-03 08:17:50 +0700

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/run/current-system/sw/bin/bash
+
+set -e
+
+mv /etc/nixos/harware-configuration.nix /etc/nixos/harware-configuration.nix.bak
+mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bak
+
+cp /etc/nixos/harware-configuration.nix hosts/hardware-configuration.nix
+
+sudo nixos-rebuild boot --flake .#io --impure
