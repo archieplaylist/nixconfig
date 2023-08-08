658d6cf39966d69fb194256433fb9ddada94254b
archieplaylist <mario.tani25@gmail.com>
2023-08-08 14:51:25 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ mv /etc/nixos/harware-configuration.nix /etc/nixos/harware-configuration.nix.bak
 mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bak
 
 cp /etc/nixos/harware-configuration.nix hosts/hardware-configuration.nix
+
 sudo nix flake update .
 # sudo nixos-rebuild boot --flake .#io --impure
